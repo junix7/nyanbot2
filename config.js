e7c1f1fa0fa29045ad179fdefbdac6d3108f1c9a
Junix <juliop.unix@gmail.com>
2021-07-20 14:58:57 -0500

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-let config = {
-  token: "ODU1ODgyMDc3MDY1MTgzMjYy.YM48TA.vfS4rJARScYhQxyuVlC_J-4oEJM", // Reemplaze esto con el token de tu de bot.
-  prefix: "nb!", // El prefix de su bot para indentificar sus comandos.
-}
-
-// Aqui exportamos el objeto config:
-module.exports = config;
